b566c63163030f8f4f760c58509258c2b7720ed5
Loïc Nachtergaele <loic@geoffrey20.local>
2021-01-06 20:13:41 +0100

    TECHNOBLADE

---
+++ view.py
@@ -0,0 +1,49 @@
+import os
+import time
+import random
+import  pyfiglet
+
+fils = os.listdir()
+files=[]
+
+for i in range(len(fils)):
+	if fils[i] != "view.py" and fils[i] != ".view.py.swo ":
+		files.append(fils[i])
+what_print = ""
+for i in range(len(files)):
+	what_print += "[%.1i] "%(i) + files[i]+" "
+x = input("auto ?y/n: ")
+
+def auto():
+	for y in range(len(files)):
+		cmd = "cat " + files[y]
+		os.system(cmd)
+		time.sleep(4)	
+
+
+if x == "y":
+	auto()
+elif x == "n":
+	while True:
+		print("\n")
+		print(what_print)
+		leng = os.get_terminal_size()
+		print("-"*leng[0])
+		inp = input("what art do you want to see: ")
+		if inp == "exit":
+			exit()
+		elif inp == "clear":
+			os.system("clear")
+		elif inp == "auto":
+			auto()
+		try:
+			cmd = "cat " + files[int(inp)]
+			os.system(cmd)
+		except:
+			pass
+
+
+l = pyfiglet.figlet_format("That's it :)",font="smkeyboard")
+print(l)
+
+
